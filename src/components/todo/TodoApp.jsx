b70acdf987bb39c301556211a460d1571f7e1222
Akash <akashsahu007@gmail.com>
2019-09-24 19:58:48 +0300

    login page commit

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -16,14 +16,14 @@ class LoginComponent extends Component{
     constructor(props)
     {
         super(props);
-        this.state={user:'scott',password:''};
+        this.state={username:'tom',password:'',loginsuccess: false, invalidcred: false};
         this.handleChange=this.handleChange.bind(this);
         this.loginclicked=this.loginclicked.bind(this);
     }
     
     handleChange(event)
     {
-        console.log(this.state);
+        
         this.setState(
             {
                 [event.target.name] : event.target.value
@@ -33,8 +33,18 @@ class LoginComponent extends Component{
     }
     loginclicked(event)
     {
-        console.log(this.state);
         
+        if(this.state.username==="tom" && this.state.password==="tom")
+        {
+            this.setState({loginsuccess: true})
+            this.setState({invalidcred: false})
+        }
+        else
+        {
+            this.setState({loginsuccess: false})
+            this.setState({invalidcred: true})
+        }
+        console.log(this.state);
 
     }
 
@@ -43,13 +53,32 @@ class LoginComponent extends Component{
     render(){
         return(
             <div>
-                
-            User Name:<input type='text' name="User" value={this.state.username} onChange={this.handleChange}/>
+                <Invalidcredcheck invalidcred={this.state.invalidcred} />
+            User Name:<input type='text' name="username" value={this.state.username} onChange={this.handleChange}/>
             Password:<input type='password' name="password" value={this.state.password} onChange={this.handleChange}/>
             <button onClick={this.loginclicked}>login</button>
+            <Loginsuccessmsg loginsuccess={this.state.loginsuccess} />
             
             </div>
         );
     }
+}
+
+function Invalidcredcheck(props)
+{
+    if(props.invalidcred){
+        return <div>Invalid creds</div>
+    }
+    else
+     return null
+}
+
+function Loginsuccessmsg(props)
+{
+    if(props.loginsuccess){
+        return <div>login success</div>
+    }
+    else
+     return null
 }
 export default TodoApp
